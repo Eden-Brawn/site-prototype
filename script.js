a98490952db63a77ca8a526452b2b5d87c3647df
Eden Brawn <edenbrawn@gmail.com>
2024-08-14 18:03:05 +0100

    clicking the hamburger makes the side menu appear

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+function hamburgerMenu(){
+    var element = document.getElementById('sidemenu');
+    element.style.display = 'block';
+
+}
 
 function myLoadFunction(){
-    
+    var element = document.getElementById('popup');
+    element.addEventListener('click', hamburgerMenu);
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
